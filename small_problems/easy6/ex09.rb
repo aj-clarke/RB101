359f49e8df948eb6edc7deb5c5ce34d9f1358151
AJ Clarke <AJGitRepo@gmail.com>
2022-12-30 22:25:49 -0500

    Adding exercise 9

---
+++ small_problems/easy6/ex09.rb
@@ -0,0 +1,10 @@
+def include?(arr, search_val)
+  arr.each { |ele| return true if ele == search_val }
+  false
+end
+
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
--- small_problems/easy6/ex09_pedac.rb
+++ small_problems/easy6/ex09_pedac.rb
@@ -1,28 +1,39 @@
 =begin
 Question:
-
+Does My List Include This?
+Write a method named include? that takes an Array and a search value as
+arguments. This method should return true if the search value is in the array,
+false if it is not. You may not use the Array#include? method in your solution.
 
 -----------------------
 Questions:
 
 Problem (Rewrite if needed else skip):
 
-Input:
-Output:
+Input: Array and search value
+Output: Boolean true or false
 Rules:
   Explicit Requirements
-
+  - Method returns TRUE if search value is in the array, false if not
+  - Cannot use Array#inclued? method
 
   Implicit Requirements
 
 -----------------------
 Examples:
-
+include?([1,2,3,4,5], 3) == true
+include?([1,2,3,4,5], 6) == false
+include?([], 3) == false
+include?([nil], nil) == true
+include?([], nil) == false
 
 -----------------------
-Data Structure:
+Data Structure: Working with input array
 Algorithm:
-**PSEUDO - Informal**
+- Create method `include?` with params `arr` and `search_val`
+- Iterate through array, and return true if current element equals `search_val`
+- Implicitly return false if not
+
 
 -----------------------
 Code:

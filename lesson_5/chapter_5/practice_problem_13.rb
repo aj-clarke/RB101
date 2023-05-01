62826f904854bbf4b082213f4dd8c8c4d4d407bf
AJ Clarke <ajclarke81@gmail.com>
2023-04-30 22:46:16 -0400

    Minor change

--- lesson_5/chapter_5/practice_problem_13.rb
+++ lesson_5/chapter_5/practice_problem_13.rb
@@ -7,7 +7,7 @@ consideration the odd numbers they contain.
 arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]]
 
 sorted = arr.sort_by do |sub_arr|
-           sub_arr.select(&:odd?)
-  end
+  sub_arr.select(&:odd?) # => [1,9][1,7],[1,3],[1,5,9]
+end
 
 p sorted
